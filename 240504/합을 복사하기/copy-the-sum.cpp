5dcc76d448c88aae944022d58b4236b95954dff1
Gasvega <102209515+Gasvega@users.noreply.github.com>
2024-05-04 23:04:14 +0900

    Title: 합을 복사하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240504/합을 복사하기/copy-the-sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 1;
+    int b = 2;
+    int c = 3;
+
+    a = 6;
+    b = 6;
+    c = 6;
+
+    cout << a << " " << b << " " << c;
+
+    return 0;
+}
