8c88e2302d0dd684cba6d59ab60061dd2d034c94
Gasvega <102209515+Gasvega@users.noreply.github.com>
2024-05-20 23:47:10 +0900

    Title: 삼항연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240520/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int ex_num;
+
+    cin >> ex_num;
+
+    (ex_num >= 100) ? cout << "pass" : cout << "failure";
+
+    return 0;
+}
