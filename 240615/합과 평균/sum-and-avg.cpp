291c0397d25f5720124f696bff1ca0fa7d5c6ad3
Gasvega <102209515+Gasvega@users.noreply.github.com>
2024-06-15 23:58:26 +0900

    Title: 합과 평균, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240615/합과 평균/sum-and-avg.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float a;
+    float b;
+
+    cin >> a;
+    cin >> b;
+
+    // cout << fixed;
+    // cout.precision(3);
+    cout << a+b << " " <<(a+b)/2;
+
+    return 0;
+}
