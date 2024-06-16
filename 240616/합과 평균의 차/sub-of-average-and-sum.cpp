bccc4028aa38d2862669d27556281575d764a67e
Gasvega <102209515+Gasvega@users.noreply.github.com>
2024-06-16 23:59:21 +0900

    Title: 합과 평균의 차, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240616/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int a;
+    int b;
+    int c;
+
+    cin >> a >> b >> c;
+
+    cout << a+b+c << endl <<
+     (a+b+c)/3 << endl << 
+     (a+b+c)-(a+b+c)/3;
+     
+    return 0;
+}
