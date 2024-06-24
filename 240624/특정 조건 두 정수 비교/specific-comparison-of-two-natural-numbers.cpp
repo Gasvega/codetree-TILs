de8698bc023a5615ccea6c036ae303f326a2e1c1
Gasvega <102209515+Gasvega@users.noreply.github.com>
2024-06-24 21:57:11 +0900

    Title: 특정 조건 두 정수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240624/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+
+    cin >> a >> b;
+
+    if(a < b) {
+        cout << 1 << " ";
+    }
+    else {
+        cout << 0 << " ";
+    }
+    if (a == b) {
+        cout << 1;
+    }
+    else {
+        cout << 0;
+    }
+
+    return 0;
+}
