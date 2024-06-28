2e9612afd07af879373ceab2a4c4555a1f729872
Gasvega <102209515+Gasvega@users.noreply.github.com>
2024-06-28 23:24:02 +0900

    Title: 4가지 관계연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240628/4가지 관계연산자/four-relational-operators.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+    
+    cin >> a >> b;
+
+    if(a >= b) {
+        cout << 1 << "\n";
+    }
+    else {
+        cout << 0 << "\n";
+    }
+    if(a > b) {
+        cout << 1 << "\n";
+    }
+    else {
+        cout << 0 << "\n";
+    }
+    if(a <= b) {
+        cout << 1 << "\n";
+    }
+    else {
+        cout << 0 << "\n";
+    }
+    if(a < b) {
+        cout << 1 << "\n";
+    }
+    else {
+        cout << 0 << "\n";
+    }
+    
+    return 0;
+}
