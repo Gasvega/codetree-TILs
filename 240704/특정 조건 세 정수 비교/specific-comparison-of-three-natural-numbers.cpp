a6b7cd721946d2242b23b363d9912c97e505d9f3
Gasvega <102209515+Gasvega@users.noreply.github.com>
2024-07-04 23:41:41 +0900

    Title: 특정 조건 세 정수 비교, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240704/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, c;
+
+    cin >> a >> b >> c;
+
+    if(a < b and a < c) {
+        cout << 1 << " ";
+    }
+    else {
+        cout << 0 << " ";
+    }
+
+    if(a == b and a == c and b == c) {
+        cout << 1;
+    }
+    else {
+        cout << 0;
+    }
+
+    return 0;
+}
