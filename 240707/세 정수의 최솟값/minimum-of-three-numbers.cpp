f2e01a9ec732ee3f9bb368fadf26bcb703522aec
Gasvega <102209515+Gasvega@users.noreply.github.com>
2024-07-07 23:55:42 +0900

    Title: 세 정수의 최솟값, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240707/세 정수의 최솟값/minimum-of-three-numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, c;
+
+    cin >> a >> b >> c;
+
+    cout << (a <= b && a <= c ? a :
+    b <= a && b <= c ? b : c);
+
+
+
+    return 0;
+}
