cc1b063b84880ac80e071c491d90802386b1cd98
Gasvega <102209515+Gasvega@users.noreply.github.com>
2024-10-10 23:56:56 +0900

    Title: 날짜 변경하여 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241010/날짜 변경하여 출력/print-date-with-different-format.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int y;
+    int m;
+    int d;
+
+    cin >> y;
+    cin.get();
+
+    cin >> m;
+    cin.get();
+
+    cin >> d;
+
+    cout << m << "-" << d <<
+         "-" << y;
+    return 0;
+}
