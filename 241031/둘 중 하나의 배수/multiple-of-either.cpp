cabeae261503de42f71bd42dba76db1f0802b79c
Gasvega <102209515+Gasvega@users.noreply.github.com>
2024-10-31 23:51:12 +0900

    Title: 둘 중 하나의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241031/둘 중 하나의 배수/multiple-of-either.cpp
+++ 241031/둘 중 하나의 배수/multiple-of-either.cpp
@@ -6,11 +6,11 @@ int main() {
 
     cin >> a;
 
-    if(a % 3 == 0 || a % 3 == 0) {
+    if(a % 3 == 0 || a % 5 == 0) {
         cout << 1;
     }
     else
     cout << 0;
-    
+
     return 0;
 }
